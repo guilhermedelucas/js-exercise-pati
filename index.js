836256b97dfb462d77dfb0527fe66d03aa3c5c35
guilhermedelucas <gdelucasde@gmail.com>
2022-05-18 18:34:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function generateRandomRgbColor() {
+	return Math.floor(Math.random() * 255) + 1;
+}
+
+function reloadColors() {
+	var boxes = document.querySelectorAll('.box');
+
+	for (var index = 0; index < boxes.length; index++) {
+		const element = boxes[index];
+
+		const colorR = generateRandomRgbColor();
+		const colorB = generateRandomRgbColor();
+		const colorG = generateRandomRgbColor();
+
+		element.style.backgroundColor = `rgb(${colorR},${colorB}, ${colorG})`;
+		// ou
+		// element.style.backgroundColor = `rgb(${generateRandomRgbColor()},${generateRandomRgbColor()}, ${generateRandomRgbColor()})`;
+	}
+}
